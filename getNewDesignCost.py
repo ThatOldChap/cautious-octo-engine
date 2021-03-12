e61ae8cbe5661569f2259ff5dcf5510df3478f49
ThatOldChap <40444184+ThatOldChap@users.noreply.github.com>
2021-03-12 12:54:09 -0500

    Update getNewDesignCost.py
    Supports different types of affected items and items that have no BOM

--- getNewDesignCost.py
+++ getNewDesignCost.py
@@ -2,16 +2,15 @@
 # getNewDesignCost.py
 # Enter an ECO# and this will open a Selenium browser, user enters login info and then
 # proceeds to the ECO and generates the affected item cost difference
-# Will need to download chrome driver, change environment path variable, download python3, pip install selenium
 
-import sys
+import sys, math
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import Select
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import StaleElementReferenceException
+from selenium.common.exceptions import NoSuchElementException
 
 # Get the ecoURL from the user when they run the script
 if len(sys.argv) > 1:
@@ -34,8 +33,8 @@ loginSelect.select_by_value("1")
 WebDriverWait(chrome, 5).until(EC.text_to_be_present_in_element((By.ID, "lblLogin_AuthenticationMethod_Description"), 'Specify network'))
 
 # Defines and fills in the user credentials
-username = '' # In the format "first last"
-password = ''
+username = '' # Enter usename here in Windows login format ex. 'michael chaplin'
+password = '' # Enter password here
 usernameElem = chrome.find_element_by_id("tbxUserName")
 usernameElem.send_keys(username)
 passwordElem = chrome.find_element_by_id("tbxPassword")
@@ -50,19 +49,16 @@ loginButton.click()
 WebDriverWait(chrome, 5).until(EC.url_contains(ecoURL))
 
 # Click on the 'Affected Items' tab and waits for webpage to switch to the affected item tab of the ECO
-newUrl = ecoURL.replace('Object', 'Reports/BOMFieldCheck') 
+newUrl = ecoURL.replace('Object', 'Reports/BOMFieldCheck')
 chrome.get(newUrl)
 WebDriverWait(chrome, 5).until(EC.text_to_be_present_in_element((By.ID, "lblPageOptionsAITitle"), 'Affected Items'))
-
-# Link: https://plm.mdsaero.com/Omnify7/Apps/Desktop/Change/Object.aspx?id=3040
-# Report: https://plm.mdsaero.com/Omnify7/Apps/Desktop/Change/Reports/BOMFieldCheck.aspx?id=3040
 
 # Change the 'Check Field' to 'DESIGN COST PER UNIT (CAD)'
 costSelect = Select(chrome.find_element_by_id("dlToolbar_FieldName"))
 costAtt = 'Attribute: DESIGN COST PER UNIT (CAD)'
 costSelect.select_by_visible_text(costAtt)
 
-# Waits for webpage to switch fields to the design cost attribute 
+# Waits for webpage to switch fields to the design cost attribute
 WebDriverWait(chrome, 5).until(EC.text_to_be_present_in_element((By.ID, "lblParentItem_Field"), costAtt))
 
 # Check the Find # box to include it as a field in the tables
@@ -75,38 +71,50 @@ if not findBox.is_selected():
 # Check to see how many affected items there are
 affItemSelect = Select(chrome.find_element_by_id("dlOptions_AffectedItems"))
 affItemsOptions = affItemSelect.options
-affItemList = []                       
+affItemList = []
 for option in affItemsOptions:
-    affItemList.append(option.text)    
-                
-print("\nAffected items are: {}".format(affItemList))
+    if not option.text[0] == 'D':
+            affItemList.append(option.text)
+    else:
+        continue
+
+print("\nAffected items are: {}\n".format(affItemList))
 costData = []
 
 # Loop through each affected item to gather the necessary data
 for item in affItemList:
 
-    # TODO: Make the table parsing happen on multiple threads to speed it up fast    
+    # TODO: Make the table parsing happen on multiple threads to speed it up fast
     print('Selecting affected item: {}...'.format(item))
-    
+
     # Select the affected item
     affItemSelect = Select(chrome.find_element_by_id("dlOptions_AffectedItems"))
     affItemSelect.select_by_visible_text(item)
     WebDriverWait(chrome, 5).until(EC.text_to_be_present_in_element((By.ID, "lblParentItem_PN"), item))
 
-    # Get the original design cost
-    originalDesignCost = int(chrome.find_element_by_id("lblParentItem_OldValue").text)
-
+    # Get the original design costo
+    originalDesignCost = round(float(chrome.find_element_by_id("lblParentItem_OldValue").text), 2)
+    
     # Gather the Original BOM into a list of lists
     cells = []
     originalBOMTable = []
     originalBOMCost = []
-    originalBOMTableElem = chrome.find_element_by_id("dgOriginalBOM")
+
+    # Finds the original BOM and skips over items like E and M parts that don't have a BOM
+    try:
+        originalBOMTableElem = chrome.find_element_by_id("dgOriginalBOM")
+    except NoSuchElementException:
+        print('Skipping over {} as it does not have a BOM\n'.format(item))
+        continue        
+    
     originalBOMCellElemList = originalBOMTableElem.find_elements_by_tag_name('td')
     print('Processing original BOM of {}...'.format(item))
 
     # Convert the WebElement items into a list of table cell texts
     for cell in originalBOMCellElemList:
         cells.append(cell.text)
+
+   #print(cells)
 
     # Parse the list of table cell texts and take the necessary data for the original BOM
     for i in range(0, len(cells), 8):
@@ -116,13 +124,16 @@ for item in affItemList:
         c7 = cells[i+7] # Design Cost
         if i < 1:
             c8 = 'Extended Cost'
+        elif c7 == ' ':
+            # Blank value is a string with 1 space in here ' '
+            continue
         else:
             c8 = float(c2) * float(c7)    # Extended Cost
             originalBOMCost.append(c8)
-        originalBOMTable.append([c1, c2, c3, c7, c8])        
+        originalBOMTable.append([c1, c2, c3, c7, c8])
 
     # Calculate the total cost of the originalBOM
-    originalBOMCostTotal = int(sum(originalBOMCost, 1))
+    originalBOMCostTotal = round(sum(originalBOMCost, 1), 2)
     print('Finished processing original BOM of {}\n'.format(item))
 
     # Gather the Proposed BOM into a list of lists
@@ -145,20 +156,23 @@ for item in affItemList:
         c8 = cells[i+8] # Design Cost
         if i < 1:
             c9 = 'Extended Cost'
+        elif c8 == '':
+            # Blank value is a an empty string here ''
+            continue
         else:
             c9 = float(c2) * float(c8)    # Extended Cost
             proposedBOMCost.append(c9)
-        proposedBOMTable.append([c1, c2, c3, c8, c9])        
+        proposedBOMTable.append([c1, c2, c3, c8, c9])
 
     # Calculate the total cost of the proposedBOM
-    proposedBOMCostTotal = int(sum(proposedBOMCost, 1))
+    proposedBOMCostTotal = round(sum(proposedBOMCost, 1), 2)
     print('Finished processing proposed BOM of {}\n'.format(item))
 
     # Store the data to iterate back through later
     costData.append([item, originalDesignCost, originalBOMCostTotal, proposedBOMCostTotal])
 
 # Close the browser
-print('Preparing summary...')
+print('Preparing summary...\n')
 chrome.quit()
 
 # Process the costData and display to the user
